c78d3642df5019e10177a3783ca9423d40ff06ff
dheile <dheile@uos.de>
2024-03-06 11:37:17 +0100

    ships update function now limits ship to screen bonds

--- ship.py
+++ ship.py
@@ -37,10 +37,10 @@ class Ship:
         """
         Update the ships position based on the movement flag
         """        
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
             
         # Update the rect position by self.x    
