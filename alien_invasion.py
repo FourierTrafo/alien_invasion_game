6f7d7755676a59b87b953141f49aaf2b98e48d05
dheile <dheile@uos.de>
2024-03-11 19:38:35 +0100

    _create_fleet() now fully fills the screen with aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -127,19 +127,26 @@ class AlienInvasion:
         #Create an alien and keep adding aliens until there's no room left
         #Spacing between aliens is one alien_width       
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
+        current_x, current_y  = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
 
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
+            current_x = alien_width       
+            current_y += 2 * alien_height
 
-        # self.aliens.add(alien)
-
+    def _create_alien(self, x_position, y_position):
+        """create an alien and place it in the row
+        """ 
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position 
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+       
 if __name__ == '__main__':
     #Make a game instance and run the game
     ai = AlienInvasion()
