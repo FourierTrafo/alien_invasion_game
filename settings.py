b2d35ffc1482b16d7bdc13f9647a45ce8a3eace0
dheile <dheile@uos.de>
2024-03-12 11:37:50 +0100

    addet switch for piercing bullets

--- settings.py
+++ settings.py
@@ -17,10 +17,11 @@ class Settings:
 
         # Bullet settings 
         self.bullet_speed = 2
-        self.bullet_width = 3
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (250, 250, 250)
         self.bullets_allowed = 3
+        self.bullets_piercing = True
 
         #Alien settings
         self.alien_scale_width = 60
