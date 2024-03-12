eb8fcd5646451e6cfc111146128bba61b19a2a0a
dheile <dheile@uos.de>
2024-03-12 12:53:43 +0100

    changed settings for faser settings

--- settings.py
+++ settings.py
@@ -18,8 +18,8 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings 
-        self.bullet_speed = 2
-        self.bullet_width = 3000
+        self.bullet_speed = 5
+        self.bullet_width = 15
         self.bullet_height = 15
         self.bullet_color = (250, 250, 250)
         self.bullets_allowed = 3
@@ -28,7 +28,7 @@ class Settings:
         #Alien settings
         self.alien_scale_width = 60
         self.alien_scale_height = 40
-        self.alien_speed = 1.0
+        self.alien_speed = 5
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
