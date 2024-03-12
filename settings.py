1bb145daeb76ec9ac5432c7be544edf6ab900c28
dheile <dheile@uos.de>
2024-03-12 11:24:28 +0100

    added settings for alien movement

--- settings.py
+++ settings.py
@@ -22,9 +22,13 @@ class Settings:
         self.bullet_color = (250, 250, 250)
         self.bullets_allowed = 3
 
-        #Aien settings
+        #Alien settings
         self.alien_scale_width = 60
         self.alien_scale_height = 40
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
 
         #Star settings
         self.star_scale_width = 10
