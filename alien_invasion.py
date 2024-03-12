03936ca9154d76818c380ea0652e6c8351a69ebf
dheile <dheile@uos.de>
2024-03-12 12:38:28 +0100

    Alien-ship collisions now are tracked properly

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,8 +2,10 @@ import sys
 import pygame
 from bullet import Bullet
 from random import randint
+from time import sleep
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from alien import Alien
 from star import Star
@@ -26,6 +28,9 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
         
+        # Create an instance for tracking game statistics
+        self.stats = GameStats(self)
+
         self.ship = Ship(self) 
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -135,11 +140,11 @@ class AlienInvasion:
         
         self._check_bullet_alien_collisions()
         
-
         if not self.aliens:
             # Destroy existing bullets and create a new fleet.
             self.bullets.empty()
             self._create_fleet()
+
 
     def _check_bullet_alien_collisions(self):
         """Resond to bullet-alien collisions
@@ -180,7 +185,28 @@ class AlienInvasion:
         """Update the positions of all aliens in the fleet
         """
         self._check_fleet_edges()
-        self.aliens.update()    
+        self.aliens.update()  
+
+        # Look for alien-ship collisions
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+                self._ship_hit()
+
+    def _ship_hit(self):
+        """Respond to the ship beeing hit
+        """
+        # Decrement ships left
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaining bullets and aliens.
+        self.bullets.empty()
+        self.aliens.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Pause
+        sleep(0.5)         
 
 
     def _check_fleet_edges(self):
