543febf6718707555005e275dae9a0366dca5258
dheile <dheile@uos.de>
2024-03-12 10:45:36 +0100

    added functions for scaling and rotating a star instance

--- star.py
+++ star.py
@@ -13,13 +13,39 @@ class Star(Sprite):
 
         #Load the alien image and set its rect attribute
         self.image = pygame.image.load('images/star.png')
-        self.image = pygame.transform.scale(self.image,
-                                             (self.settings.star_scale_width,
-                                               self.settings.star_scale_height))
+
+        #Scale the star to default size
+        self.scale(self.settings.star_scale_width, 
+                   self.settings.star_scale_height)
+     
+        #Rotate the star by the default angle
+        self.rotate(self.settings.star_rotation)
+        
         self.rect = self.image.get_rect()
 
         #Start each star near the top left of the screen
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
+
+    def rotate(self, rotation_angle):
+      """Rotate the star image by the given angle
+
+      Args:
+          rotation_angle (int): Rotation angle
+      """        
+      self.angle = rotation_angle
+      self.image = pygame.transform.rotate(self.image, self.angle)
+
+    def scale(self, width, height):
+      """Scale the star
+
+      Args:
+          width (int): Width the star is scaled to
+          height (int): Height the star is scaled to
+      """       
+      self.image = pygame.transform.scale(self.image, (width, height))
+                                           
+
+        
 
         
