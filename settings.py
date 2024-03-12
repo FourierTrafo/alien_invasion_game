fa45483b21d8e0740e13cb7bbf4dae480c7187cb
dheile <dheile@uos.de>
2024-03-12 17:01:01 +0100

    Added dynamic settings and a function for inkrementing game speed

--- settings.py
+++ settings.py
@@ -11,31 +11,52 @@ class Settings:
         self.bg_color = (0, 13, 33)
         self.frame_rate = 60
 
-        # Ship settings
-        self.ship_speed = 10
+        # Static Ship settings
         self.ship_scale_width = 40
         self.ship_scale_height = 80
-        self.ship_limit = 3
+        self.ship_limit = 2
 
-        # Bullet settings 
-        self.bullet_speed = 5
-        self.bullet_width = 15
+        # Static Bullet settings 
+        self.bullet_width = 300
         self.bullet_height = 15
-        self.bullet_color = (250, 250, 250)
-        self.bullets_allowed = 3
-        self.bullets_piercing = True
-
+        
         #Alien settings
         self.alien_scale_width = 60
         self.alien_scale_height = 40
         self.alien_speed = 5
-        self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left.
-        self.fleet_direction = 1
+        self.fleet_drop_speed = 1
 
         #Star settings
         self.star_scale_width = 10
         self.star_scale_height = 10
         self.star_rotation = 0
         self.stars_allowed = 50
-        
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game
+        """ 
+        # Dynamic Ship settings
+        self.ship_speed = 10
+
+        # Dynamic Bullet settings
+        self.bullet_speed = 5
+        self.bullets_piercing = False
+        self.bullet_color = (250, 250, 250)
+        self.bullets_allowed = 3
+
+        # Dynamic alien settings
+        self.alien_speed = 5
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings
+        """        
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
