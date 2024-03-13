55521bf59ae73fb29631b338046409efe5d4c5f0
dheile <dheile@uos.de>
2024-03-13 09:52:21 +0100

    all time high score is loaded upon initiliasation

--- game_stats.py
+++ game_stats.py
@@ -1,4 +1,5 @@
-
+import json
+import os
 
 class GameStats:
     """Track statistics for alien invasion
@@ -12,7 +13,7 @@ class GameStats:
         """
         self.settings = ai_game.settings
         self.reset_stats()
-        self.high_score = 0
+        self.prep_high_score()
         
 
 
@@ -22,3 +23,16 @@ class GameStats:
         self.ships_left = self.settings.ship_limit
         self.score = 0                    
         self.level = 1
+    
+    def prep_high_score(self):
+        """Loads the high score from previous games
+        """ 
+        high_score_save = 'high_score.json'
+
+        if os.path.exists(high_score_save):
+            with open(high_score_save, "r") as json_datei:
+                data = json.load(json_datei)
+            
+            self.high_score = data
+        else:
+            self.high_score = 0
